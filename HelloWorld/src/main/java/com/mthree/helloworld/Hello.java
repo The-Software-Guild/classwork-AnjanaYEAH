3e373333eba4c2c6c5a5c15fad6b847f31c83364
AJ <cia@cia.gov>
2022-02-16 12:11:42 +0000

    hello

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mthree.helloworld;
+
+/**
+ *
+ * @author anjanaratnayake
+ * @email anjana06@protonmail.com
+ * @date 16 Feb 2022s
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HEllo world");
+        System.out.println("helo");
+    }
+
+}
